c9b9bd5303cad9ca21ba7e9b34fa293d70782ea4
Max <maximiliano.riffo@nubox.com>
2020-04-30 12:43:12 -0400

    Primera version

---
+++ download.py
@@ -0,0 +1,53 @@
+import os
+import urllib.parse as urlparse
+from os import path
+from urllib.parse import parse_qs
+
+import click
+import wget
+from mhmovie.code import *
+
+#TODO: Leer el archivo de links como csv y al archivo agregarle una columna
+    #  con el nombre de salida del archivo. 
+def validate_folder(folder):
+    if not path.exists(folder):
+        os.mkdir(folder) 
+
+def download(path, output=os.path.join(os.getcwd(),"output")):
+    validate_folder(output)
+    
+    __file = wget.download(path, out=output)
+    return (__file)
+
+def change_extension(path):
+    base = os.path.splitext(path)[0]
+    new_file = os.rename(path, base + '.mp3')
+    return (base + '.mp3')
+
+@click.command()
+@click.option('--file', default=None,  help='path file with links to download ig live')
+def read_file_with_link(file):
+    with open(file, encoding="utf-8") as links:
+        for pos , row in enumerate(links):
+           data_url = urlparse.urlparse(urlparse.unquote(row)).query
+           paths = data_url.split('file_v=')[1].split('&file_a=')
+           output = os.path.join(os.getcwd(),"output")
+           join_folder = os.path.join(output, "join")
+           validate_folder(join_folder)           
+           join_video = os.path.join(join_folder,  str(pos) + "_video.mp4")
+           file_video = download(paths[0], output)
+           file_audio = change_extension(download(paths[1], output))
+           join_audio_and_video(file_audio, file_video, join_video)
+           #rint(os.getcwd())
+
+def join_audio_and_video(audio, video, save):
+    m = movie(video)
+    mu = music(audio)
+    mu.Aconvert()#convert wav to mp3
+    final = m+mu
+
+    final.save(save)
+
+
+if __name__ == "__main__":
+    read_file_with_link()
